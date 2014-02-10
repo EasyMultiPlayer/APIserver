76bd39a1e67e023dc27984d926f31f6a164cadae
Suresh Alse <alseambusher@gmail.com>
2014-02-10 12:04:30 +0530

    module added

--- lib/transport.py
+++ lib/transport.py
@@ -10,6 +10,7 @@ import db
 from basic import data_filter
 from keys import session_key_gen
 
+
 class Transport():
     logs = []
     data_pub = {} # this should have 'session_key':'<data_to_be_pushed>' dictionary
@@ -49,15 +50,15 @@ class Transport():
         while True:
             iterator = self.connected.keys()[:]
             for client_shared_key in iterator:
-                if  time.time() - self.connected[client_shared_key] > float(config.ALIVE_TIMEOUT):
+                if time.time() - self.connected[client_shared_key] > float(config.ALIVE_TIMEOUT):
                     try:
-                        client=db.get_user_from_shared_key(client_shared_key)
-                        print logging.debug("disconnect "+client.user.shared_key,"[INFO]")
-                        data={'shared_key':client.user.shared_key}
-                        self.send(data,action=actions.dead,key=client.server_shared_key)
+                        client = db.get_user_from_shared_key(client_shared_key)
+                        print logging.debug("disconnect " + client.user.shared_key, "[INFO]")
+                        data = {'shared_key': client.user.shared_key}
+                        self.send(data, action=actions.dead, key=client.server_shared_key)
                         del self.connected[client.user.shared_key]
                     except:
-                        # todo take care of servers not responding also
+                        # todo take care of servers not responding also. You have to remove all the clients if the server is not responding
                         pass
             time.sleep(5)
 
@@ -67,18 +68,18 @@ class Transport():
         # from game_server or client: api_key
         if _action == actions.connect:
             end_user = db.get_user_from_api_key(data['api_key'])
-            self.connected[end_user.user.shared_key]=time.time()
+            self.connected[end_user.user.shared_key] = time.time()
             print logging.debug("Connect " + data['api_key'], "[Info]")
 
         elif _action == actions.alive:
-            self.connected[data['shared_key']]=float(data['time'])
+            self.connected[data['shared_key']] = float(data['time'])
 
         # from client api_key,
         elif _action == actions.get_games:
             # TODO if client doesnt exist then raise error
             client = db.get_user_from_api_key(data['api_key'])
             print logging.debug(actions.get_games, '[Info]')
-            self.send({'shared_key': client.user.shared_key,}, action=actions.get_games, key=client.server_shared_key)
+            self.send({'shared_key': client.user.shared_key, }, action=actions.get_games, key=client.server_shared_key)
 
         # from game server
         elif _action == actions.game_list:
@@ -93,7 +94,7 @@ class Transport():
         # from client select game shared_key
         elif _action == actions.select_game:
             client = db.get_user_from_shared_key(data['shared_key'])
-            self.send(data,action=actions.select_game, key=client.server_shared_key)
+            self.send(data, action=actions.select_game, key=client.server_shared_key)
 
 
         # from client new game: shared_key
@@ -101,22 +102,22 @@ class Transport():
             client = db.get_user_from_shared_key(data['shared_key'])
             data['session_key'] = session_key_gen()
             # send server
-            self.send(data,action=actions.new_game, key=client.server_shared_key)
-            data=data_filter(data,['shared_key',])
+            self.send(data, action=actions.new_game, key=client.server_shared_key)
+            data = data_filter(data, ['shared_key', ])
             # send to client
-            self.send(data,action=actions.game_session, key=client.user.shared_key)
+            self.send(data, action=actions.game_session, key=client.user.shared_key)
 
         # from client, to make things faster server_shared_key is also sent
         elif _action == actions.event:
-            key=data['server_shared_key']
-            data=data_filter(data,['server_shared_key'])
-            self.send(data,action=actions.event,key=key)
+            key = data['server_shared_key']
+            data = data_filter(data, ['server_shared_key'])
+            self.send(data, action=actions.event, key=key)
 
         # from server. forward this to all clients
         elif _action == actions.game_state:
-            session_key=data['session_key']
-            data=data_filter(data,['session_key'])
-            self.send(data,action=actions.game_state,key=session_key)
+            session_key = data['session_key']
+            data = data_filter(data, ['session_key'])
+            self.send(data, action=actions.game_state, key=session_key)
 
     def pull(self, port):
         context = zmq.Context()
@@ -127,16 +128,14 @@ class Transport():
 
         while True:
             string = socket.recv()
-            data=json.loads(string)
+            data = json.loads(string)
             if data['action'] != actions.alive:
                 print logging.debug(string, '[PULL:' + port + ']')
             thread = threading.Thread(target=self.pull_processor, args=(data,))
             thread.start()
 
-            # to decrease CPU usage
             time.sleep(0.001)
 
-    # TODO make this multi threaded
     def publish(self):
         context = zmq.Context()
         socket = context.socket(zmq.PUB)
@@ -151,5 +150,4 @@ class Transport():
                 socket.send(str(session_key + " " + data_queue[session_key]))
                 del (self.data_pub[session_key])
 
-            # to decrease CPU usage
             time.sleep(0.001)
